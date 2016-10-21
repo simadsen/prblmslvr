07a1a7618e9906587c1fa0f059443771cd94099b
simadsen <hi@simonmadsen.com>
2016-10-21 15:19:34 +0200

    Add logic to hide inactive cards

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,23 @@
 function main() {
+  $('.deck > ul > li').hide();
+  var currentDeck = "";
+  var deckCount = countCardsInDeck();
+  var activeCard = 0;
+
+  function countCardsInDeck (){
+    return document.querySelectorAll(".deck > ul > li").length;
+  };
+
+  function navigateCards () {
+
+  };
+
+  function showActiveCard (){
+
+  };
+
+  console.log("Number of cards: "+deckCount);
+  console.log("Active card: "+activeCard);
 
 }
 
